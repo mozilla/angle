dd6213dafac2dd5714b5161dd34e8c8479216552
jdashg <jdashg+github@gmail.com>
2015-08-07 15:41:16 -0400

    Fix BGRA in CopyTexSubImage.
    Mozilla bug 912196.
    r=kamidphish

--- src/libANGLE/validationES2.cpp
+++ src/libANGLE/validationES2.cpp
@@ -423,7 +423,8 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
             if (colorbufferFormat != GL_ALPHA8_EXT &&
                 colorbufferFormat != GL_RGBA4 &&
                 colorbufferFormat != GL_RGB5_A1 &&
-                colorbufferFormat != GL_RGBA8_OES)
+                colorbufferFormat != GL_RGBA8_OES &&
+                colorbufferFormat != GL_BGRA8_EXT)
             {
                 context->recordError(Error(GL_INVALID_OPERATION));
                 return false;
@@ -436,7 +437,8 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                   colorbufferFormat != GL_RGB8_OES &&
                   colorbufferFormat != GL_RGBA4 &&
                   colorbufferFormat != GL_RGB5_A1 &&
-                  colorbufferFormat != GL_RGBA8_OES)
+                  colorbufferFormat != GL_RGBA8_OES &&
+                  colorbufferFormat != GL_BGRA8_EXT)
               {
                   context->recordError(Error(GL_INVALID_OPERATION));
                   return false;
@@ -450,6 +452,7 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                   colorbufferFormat != GL_RGBA4 &&
                   colorbufferFormat != GL_RGB5_A1 &&
                   colorbufferFormat != GL_RGBA8_OES &&
+                  colorbufferFormat != GL_BGRA8_EXT &&
                   colorbufferFormat != GL_R32F &&
                   colorbufferFormat != GL_RG32F &&
                   colorbufferFormat != GL_RGB32F &&
@@ -466,6 +469,7 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                   colorbufferFormat != GL_RGBA4 &&
                   colorbufferFormat != GL_RGB5_A1 &&
                   colorbufferFormat != GL_RGBA8_OES &&
+                  colorbufferFormat != GL_BGRA8_EXT &&
                   colorbufferFormat != GL_RG32F &&
                   colorbufferFormat != GL_RGB32F &&
                   colorbufferFormat != GL_RGBA32F)
@@ -480,6 +484,7 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                 colorbufferFormat != GL_RGBA4 &&
                 colorbufferFormat != GL_RGB5_A1 &&
                 colorbufferFormat != GL_RGBA8_OES &&
+                colorbufferFormat != GL_BGRA8_EXT &&
                 colorbufferFormat != GL_RGB32F &&
                 colorbufferFormat != GL_RGBA32F)
             {
@@ -492,6 +497,7 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
             if (colorbufferFormat != GL_RGBA4 &&
                 colorbufferFormat != GL_RGB5_A1 &&
                 colorbufferFormat != GL_RGBA8_OES &&
+                colorbufferFormat != GL_BGRA8_EXT &&
                 colorbufferFormat != GL_RGBA32F)
             {
                 context->recordError(Error(GL_INVALID_OPERATION));
